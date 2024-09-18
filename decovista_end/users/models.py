534db81f67ef4ece405a44971e5ac9b8d8a83a16
great-special <sirgreatspecial@gmail.com>
2024-09-18 17:11:28 +0100

    added UserDetails Model in the users app

--- decovista_end/users/models.py
+++ decovista_end/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+
+class UserDetails(models.Model):
+    ROLES = (
+        ('Designer', 'Designer'),
+        ('User', 'User')
+    )
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_details')
+    role = models.CharField(max_length=100, choices=ROLES, default='User')
+    contact_number = models.CharField(max_length=15)
+    
