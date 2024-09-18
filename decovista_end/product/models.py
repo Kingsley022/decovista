42874b71ad0a6df1aaf10bb4bfe7ab2d83c0c14a
RamzypeeTech <okerekeprince111@gmail.com>
2024-09-18 22:20:55 +0100

    modified appointment models and added urls

--- decovista_end/product/models.py
+++ decovista_end/product/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
+
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=100)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='products')
+    brand = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='product_images/')
+
+    def __str__(self):
+        return self.product_name
+
+class Review(models.Model):
+    review_id = models.AutoField(primary_key=True)
+    product = models.ForeignKey('Products', on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=1)  
+    comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review {self.review_id} for {self.product.product_name}"
