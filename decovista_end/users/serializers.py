8a7c7c40e7726d0ac06ca730c136597484608097
great-special <sirgreatspecial@gmail.com>
2024-09-20 01:10:47 +0100

    added product logic

--- decovista_end/users/serializers.py
+++ decovista_end/users/serializers.py
@@ -1,34 +1,49 @@
-from rest_framework import serializers
-from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer,  UserSerializer as BaseUserSerializer
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer, UserSerializer as BaseUserSerializer
 from .models import UserDetails, InteriorDesigner
 
+class UserDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserDetails
+        fields = ['role', 'contact_number', 'address']
 
-class UserCreateSerializer(BaseUserCreateSerializer):
+class InteriorDesignerSerializer(serializers.ModelSerializer):
+    user_details = UserDetailsSerializer()
+
+    class Meta:
+        model = InteriorDesigner
+        fields = ['years_of_experience', 'specialization', 'portfolio', 'user_details']
+
+class CustomUserCreateSerializer(BaseUserCreateSerializer):
+    user_details = UserDetailsSerializer(required=False)
+    designer = InteriorDesignerSerializer(required=False)
+
     class Meta(BaseUserCreateSerializer.Meta):
-        fields = ['id', 'username', 'password', 'first_name', 'last_name', 'email']
-        
-        
+        fields = BaseUserCreateSerializer.Meta.fields + ('user_details', 'designer')
 
-class UserSerializer(BaseUserSerializer):
-    class Meta(BaseUserCreateSerializer.Meta):
-        fields = [
-            'id', 'first_name',
-            'last_name', 'email',
-            'username', 'is_active',
-        ]
+    def create(self, validated_data):
+        user_details_data = validated_data.pop('user_details', None)
+        designer_data = validated_data.pop('designer', None)
+        user = super().create(validated_data)
 
-    # def validate(self, attrs):
-    #     validated_attrs = super().validate(attrs)
-    #     username = validated_attrs.get('username')
-        
-    #     user = user.objects.get(username=username)
-        
-    #     if user.is_deactivated():
-    #         raise ValidationError()
-        
-        
-        
+        if user_details_data:
+            user_details = UserDetails.objects.create(user_id=user, **user_details_data)
+
+            if designer_data:
+                InteriorDesigner.objects.create(user_details=user_details, **designer_data)
+
+        return user
+
+class CustomUserSerializer(BaseUserSerializer):
+    # user_details = UserDetailsSerializer(read_only=True, source='user_details.first') # Use .first() to get the first UserDetails object
+    user_details = UserDetailsSerializer(read_only=True, many=True) # Use many=True to get multiple related objects,
+    designer = InteriorDesignerSerializer(read_only=True, source='user_details.designer')
+
+    class Meta(BaseUserSerializer.Meta):
+        fields = BaseUserSerializer.Meta.fields + ('user_details', 'designer')
+
+
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
@@ -47,10 +62,3 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
        
 
         return data
-
-class UserDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserDetails
-        fields = [
-            'role', 'contact_number', 'address'
-        ]
